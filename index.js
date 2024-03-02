cd66b290d9befd1285e64d62883da8d57568812a
kungale <kungale11@gmail.com>
2024-03-02 11:08:17 +0100

    model

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 import dotenv from "dotenv";
 import morgan from "morgan";
-import express from "express"; 
-import cors from 'cors';
-import connectDB from './utils/connectDB.js';
-
+import express from "express";
+import cors from "cors";
+import connectDB from "./utils/connectDB.js";
 
 dotenv.config();
 const app = express();
 app.use(cors("*"));
 app.use(morgan("dev"));
 app.use(express.json());
-
-
-
 
 connectDB();
 
