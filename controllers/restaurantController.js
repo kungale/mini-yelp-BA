eca46177715a5447ebff02cc1c6df2ef3f4a3383
kungale <kungale11@gmail.com>
2024-03-02 13:45:17 +0100

    getById

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -20,23 +20,23 @@ export const handleAddRestaurant = async (req, res) => {
     console.log("this is add restaurant", req.body);
 
     const restaurant = await Restaurant.create({
-        name: req.body.name,
-        image_URL: req.body.image_URL,
-        location: {
-          address: req.body.location.address,
-          city: req.body.location.city,
-          state: req.body.location.state,
-          zip_code: req.body.location.zip_code,
-        },
-        tags: req.body.tags,
-        rating: req.body.rating,
-        reviews: req.body.reviews.map(review => ({
-          id: review.id,
-          user_id: review.user_id,
-          text: review.text,
-          rating: review.rating,
-        })),
-      });
+      name: req.body.name,
+      image_URL: req.body.image_URL,
+      location: {
+        address: req.body.location.address,
+        city: req.body.location.city,
+        state: req.body.location.state,
+        zip_code: req.body.location.zip_code,
+      },
+      tags: req.body.tags,
+      rating: req.body.rating,
+      reviews: req.body.reviews.map((review) => ({
+        id: review.id,
+        user_id: review.user_id,
+        text: review.text,
+        rating: review.rating,
+      })),
+    });
 
     res.send({ success: true });
   } catch (error) {
@@ -44,30 +44,47 @@ export const handleAddRestaurant = async (req, res) => {
   }
 };
 
-
 export const handleGetByCity = async (req, res) => {
-    try {
-        console.log("get one restaurant", req.query);
+  try {
+    console.log("get one restaurant", req.query);
 
-        const restaurant = await Restaurant.find({"location.city": new RegExp(req.query.city, 'i')});
-        console.log("restaurant", restaurant);
-        res.send(restaurant);
-    } catch (error) {
-        console.log("error in get one post: ", error.message);
+    const restaurant = await Restaurant.find({
+      "location.city": new RegExp(req.query.city, "i"),
+    });
+    console.log("restaurant", restaurant);
+    res.send(restaurant);
+  } catch (error) {
+    console.log("error in get one post: ", error.message);
 
-        res.status(500).send({success: false, error: error.message});
-    }
-}
+    res.status(500).send({ success: false, error: error.message });
+  }
+};
 export const handleGetByTag = async (req, res) => {
-    try {
-        console.log("get one restaurant", req.query);
+  try {
+    console.log("get one restaurant", req.query);
 
-        const restaurant = await Restaurant.find({"tags": new RegExp(req.query.tag, 'i')});
-        console.log("restaurant", restaurant);
-        res.send(restaurant);
-    } catch (error) {
-        console.log("error in get one post: ", error.message);
+    const restaurant = await Restaurant.find({
+      tags: new RegExp(req.query.tag, "i"),
+    });
+    console.log("restaurant", restaurant);
+    res.send(restaurant);
+  } catch (error) {
+    console.log("error in get one post: ", error.message);
 
-        res.status(500).send({success: false, error: error.message});
-    }
-}
+    res.status(500).send({ success: false, error: error.message });
+  }
+};
+
+export const handleGetById = async (req, res) => {
+  try {
+    console.log("get one restaurant", req.query);
+
+    const restaurant = await Restaurant.findById(req.query.id);
+    console.log("restaurant", restaurant);
+    res.send(restaurant);
+  } catch (error) {
+    console.log("error in get one post: ", error.message);
+
+    res.status(500).send({ success: false, error: error.message });
+  }
+};
--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { handleGetAllRestaurants, handleAddRestaurant,handleGetByTag,  handleGetByCity} from "../controllers/restaurantController.js"
+import {
+  handleGetAllRestaurants,
+  handleAddRestaurant,
+  handleGetByTag,
+  handleGetByCity,
+  handleGetById,
+} from "../controllers/restaurantController.js";
 
 const router = express.Router();
 
-
 router.get("/restaurant/all", handleGetAllRestaurants);
 router.post("/restaurant/add", handleAddRestaurant);
-router.get("/restaurant/getByCity", handleGetByCity)
-router.get("/restaurant/getByTag", handleGetByTag)
+router.get("/restaurant/getByCity", handleGetByCity);
+router.get("/restaurant/getByTag", handleGetByTag);
+router.get("/restaurant/getById", handleGetById);
+
 export default router;
